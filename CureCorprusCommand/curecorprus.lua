986da9547e15a814f1103fe5ee8c807b0ec4ebc1
Schnibbsel <40900062+Schnibbsel@users.noreply.github.com>
2018-10-10 16:45:36 +0200

    change

--- CureCorprusCommand/curecorprus.lua
+++
@@ -1,5 +0,0 @@
-elseif cmd[1] == "curecorprus" then 
-	if Players[pid]~= nil and Players[pid]:IsLoggedIn() then
-		myMod.RunConsoleCommandOnPlayer(pid, "player->removespell corprus")
-	end
-	
--- FixedDeathDrop/Readme.md
+++
@@ -1,3 +0,0 @@
-https://github.com/David-AW/tes3mp-safezone-dropitems/blob/master/deathdrop.lua
-
-this is fixed version for 0.6.2-hotfixed
